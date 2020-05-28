1cdcb394aba6f577a24e573c98f7fe40103d2476
zhangxu <2590000714@qq.com>
2020-05-28 18:57:06 +0800

    用户的schema

--- app/models/users.js
+++ app/models/users.js
@@ -5,6 +5,11 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false // 密码不被查询出去
   }
 })
 
